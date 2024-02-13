733645e5d81a47f1fdc0830612ed7df72cc50b8b
cristian <ch374185@gmail.com>
2024-02-13 13:57:20 -0600

    Adding home_screen.dart

--- lib/main_app.dart
+++ lib/main_app.dart
@@ -1,16 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:practica3/home_screen.dart';
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return const MaterialApp(home: HomeScreen());
   }
 }
