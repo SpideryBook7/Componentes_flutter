71d8dde917e84e14130ebc91613921d576d0448b
cristian <ch374185@gmail.com>
2024-02-14 13:50:42 -0600

    Adding Listile to the ListView

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -6,28 +6,41 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(109, 87, 235, 255),
       appBar: AppBar(
         title: const Text('Componentes de flutter'),
+        backgroundColor: Colors.blue,
       ),
-      body: ListView(children: const [
-        Text('Hellow Jamon'),
-        Text('Hellow Jamon'),
-        Text('Hellow Jamon'),
-        Text('Hellow Jamon'),
-        Text('Hellow Jamon'),
-        Text('Hellow Jamon'),
-        Text('Hellow Jamon'),
-        Text('Hellow Jamon'),
-        Text('Hellow Jamon'),
-        Text('Hellow Jamon'),
-        Text('Hellow Jamon'),
-        Text('Hellow Jamon'),
-        Text('Hellow Jamon'),
-        Text('Hellow Jamon'),
-        Text('Hellow Jamon'),
-        Text('Hellow Jamon'),
-        Text('Hellow Jamon'),
-      ]),
+      body: ListView(
+        children: const [
+          ListTile(
+            leading: Icon(Icons.account_circle_outlined),
+            title: Text('Hellow ',
+                style: TextStyle(color: Color.fromARGB(255, 38, 71, 255))),
+            subtitle: Text('23-23-23 - Elein',
+                style: TextStyle(color: Color.fromARGB(255, 141, 26, 255))),
+            trailing: Icon(Icons.arrow_circle_right),
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.place),
+            title: Text('How are you from ',
+                style: TextStyle(color: Color.fromARGB(255, 27, 110, 255))),
+            subtitle: Text('23-23-23 - New York',
+                style: TextStyle(color: Color.fromARGB(255, 141, 26, 255))),
+            trailing: Icon(Icons.arrow_circle_right),
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.place),
+            title: Text('Place',
+                style: TextStyle(color: Color.fromARGB(255, 21, 107, 255))),
+            subtitle: Text('23-23-23 - Arizona',
+                style: TextStyle(color: Color.fromARGB(255, 141, 26, 255))),
+            trailing: Icon(Icons.arrow_circle_right),
+          ),
+        ],
+      ),
     );
   }
 }
